50219c1fd8f9f1fae71b9be2ab6d8d78f5db67a6
Javier <javier.smith.dlc@gmail.com>
2020-12-02 19:34:07 -0300

    Adds code for stereo image capture and calibration

---
+++ src/util/generate_stereo_pairs.sh
@@ -0,0 +1,10 @@
+python capture_stereo.py --output_dir ../../stereo_chessboard --delay_seconds 8
+python capture_stereo.py --output_dir ../../stereo_chessboard --delay_seconds 4
+python capture_stereo.py --output_dir ../../stereo_chessboard --delay_seconds 4
+python capture_stereo.py --output_dir ../../stereo_chessboard --delay_seconds 4
+python capture_stereo.py --output_dir ../../stereo_chessboard --delay_seconds 4
+python capture_stereo.py --output_dir ../../stereo_chessboard --delay_seconds 4
+python capture_stereo.py --output_dir ../../stereo_chessboard --delay_seconds 4
+python capture_stereo.py --output_dir ../../stereo_chessboard --delay_seconds 4
+
+
