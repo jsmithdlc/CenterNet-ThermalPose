544b66c33cce3e01eace7dfce399426216735b89
Javier <javier.smith.dlc@gmail.com>
2021-01-14 12:53:22 -0300

    fully working centernet

--- CenterNet/experiments/run_demo.sh
+++ CenterNet/experiments/run_demo.sh
@@ -1,3 +1,3 @@
 cd src
-python demo.py multi_pose --demo ../data/thermal_pose/test --load_model ../exp/multi_pose/dla_1x_thermalgan/model_140.pth
+python demo.py multi_pose --demo ../data/thermal_pose/val/ --load_model ../models/multi_pose_dla_3x_gray_384_0frz.pth
 cd ..
